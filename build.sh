7a33c80b8c6ca049e76c4cb02c79f097e2eea018
Asamer <asam_hu@eoc-opm-t_05-vm.eoc.dlr.de>
2016-11-21 13:27:46 +0000

    Changed docker-img tags.

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 cd ../..
-docker build -t asamerh4/mesos-build-img-$MESOS_BUILD build-img/
+docker build -t asamerh4/mesos-build-img:$MESOS_BUILD build-img/
 
-docker run --rm -it -e MAKEFLAGS=-j$NPROC -v $ROOT_DIR/build-img/rpm:/out asamerh4/mesos-build-img-$MESOS_BUILD /bin/bash -c "./mesos-rpm-packaging/build_mesos --src-dir /mesos;  cp /mesos-rpm-packaging/pkg.rpm /out"
+docker run --rm -it -e MAKEFLAGS=-j$NPROC -v $ROOT_DIR/build-img/rpm:/out asamerh4/mesos-build-img:$MESOS_BUILD /bin/bash -c "./mesos-rpm-packaging/build_mesos --src-dir /mesos;  cp /mesos-rpm-packaging/pkg.rpm /out"
 
-docker build -t asamerh4/mesos-batch-$MESOS_BUILD .
+docker build -t asamerh4/mesos-batch:$MESOS_BUILD .
 
-echo -e ${YELLOW}"**build finished -> use: docker run --rm -it asamerh4/mesos-batch-$MESOS_BUILD mesos-batch --h" ${NC}
+echo -e ${YELLOW}"**build finished -> use: docker run --rm -it asamerh4/mesos-batch:$MESOS_BUILD mesos-batch --h" ${NC}
