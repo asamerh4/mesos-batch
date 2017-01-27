fd33db8424e5185259bb624c0f6e11f2c8abcd8f
Hubert Asamer <hubert.asamer@dlr.de>
2017-01-27 19:03:06 +0000

    update

---
+++ examples/test-createVolume-and-run.sh
@@ -0,0 +1,1 @@
+mesos-batch --master=localhost:5050 --task_list=file:///path/to/taskgroup.json --persistent_volume=true --persistent_volume_resource=file:///path/to/persistent_volume_resource.json --framework_name=S2-MSI-DOCKER_032_poc-de --principal=test --role=test --remove_persistent_volume=false
